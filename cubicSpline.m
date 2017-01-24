8954c398ec018e13925b27d6d8ecc8dcb291fb2e
Rafael Bachmann <triolenzunge@gmail.com>
2017-01-24 19:00:17 +0100

    better variable name for index

--- cubicSpline.m
+++ cubicSpline.m
@@ -1,4 +1,4 @@
-function y_s = cubicSpline(a, b, c, d, x_i, x_ii)
-  x_s = x_i:0.05:x_ii-0.05;
+function y_s = cubicSpline(a, b, c, d, x_i, x_j)
+  x_s = x_i:0.05:x_j-0.05;
   y_s = a + b * (x_s - x_i) + c * (x_s - x_i) .^ 2 + d * (x_s - x_i) .^ 3;
 endfunction
